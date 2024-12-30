e692465f30b52654d315df42de27dcdb7f8f0d6b
Paul H Alfille <paul.alfille@gmail.com>
2024-12-30 15:43:00 -0500

    Update query_mod.js fir better parse and prune queries. Little hand building needed

--- js/query_mod.js
+++ js/query_mod.js
@@ -13,163 +13,96 @@
 // Create pouchdb indexes.
 // Used for links between records and getting list of choices
 // change version number to force a new version
-export function createQueries() {
-    let ddoclist = [
-    {
-        _id: "_design/qGlaze",
-        version: 3,
-        views: {
-            qGlaze: {
-                map: function (doc) {
-                    if ("glaze" in doc) {
-                        doc.glaze.forEach( g => {
-                            if ( "type" in g ) {
-                                emit( g.type ) ;
-                            }
-                        }) ;
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qKiln",
-        version: 3,
-        views: {
-            qKiln: {
-                map: function (doc) {
-                    if ("kilns" in doc) {
-                        doc.kilns.forEach( g => {
-                            if ( "kiln" in g ) {
-                                emit( g.kiln ) ;
-                            }
-                        }) ;
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qCone",
-        version: 3,
-        views: {
-            qCone: {
-                map: function (doc) {
-                    if ("kilns" in doc) {
-                        doc.kilns.forEach( g => {
-                            if ( "cone" in g ) {
-                                emit( g.cone ) ;
-                            }
-                        }) ;
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qProcess",
-        version: 3,
-        views: {
-            qProcess: {
-                map: function (doc) {
-                    if ("process" in doc) {
-                        doc.process.forEach( g => {
-                            if ( "type" in g ) {
-                                emit( g.type ) ;
-                            }
-                        }) ;
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qType" ,
-        version: 3,
-        views: {
-            qType: {
-                map: function( doc ) {
-                    if ( "type" in doc ) {
-                        emit( doc.type );
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qSeries" ,
-        version: 3,
-        views: {
-            qSeries: {
-                map: function( doc ) {
-                    if ( "series" in doc ) {
-                        emit( doc.series );
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qArtist" ,
-        version: 3,
-        views: {
-            qArtist: {
-                map: function( doc ) {
-                    if ( "artist" in doc ) {
-                        emit( doc.artist );
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-        _id: "_design/qLocation" ,
-        version: 3,
-        views: {
-            qLocation: {
-                map: function( doc ) {
-                    if ( "location" in doc ) {
-                        emit( doc.location );
-                    }
-                }.toString(),
-                reduce: '_count',
-            },
-        },
-    },
-    {
-		_id: "_design/qPictures",
-		version: 3,
-		views: {
-			qPictures: {
-				map: function(doc) { 
-					emit( doc._id, ('images' in doc) ? doc.images.length : 0 ); 
-				}.toString(), 
-				reduce: '_stats',
+
+export {
+	Query,
+};
+    
+class Query {
+	static version = 1 ; // change to force renewal (value is arbitrary)
+	constructor(struct) {
+		this.version = `${Query.version}` ;
+		const queries = this.struct_parse(struct) ; // query entries
+		// add image statistics
+		queries.push( ({
+			_id: "_design/qPictures",
+			views: {
+				qPictures: {
+					map: function(doc) { 
+						emit( doc._id, ('images' in doc) ? doc.images.length : 0 ); 
+					}.toString(), 
+					reduce: '_stats',
+				},
 			},
-		},
-	}, 	
-    ];
-    Promise.all( ddoclist.map( (ddoc) => {
-        objectDatabase.db.get( ddoc._id )
-        .then( doc => {
-            if ( ddoc.version !== doc.version ) {
-                ddoc._rev = doc._rev;
-                return objectDatabase.db.put( ddoc );
-            } else {
-                return Promise.resolve(true);
-            }
-            })
-        .catch( () => {
-            // assume because this is first time and cannot "get"
-            return objectDatabase.db.put( ddoc );
-            });
-        }))
-    .catch( (err) => objectLog.err(err) );
+		}) );
+		Promise.all( queries.map( (ddoc) => {
+			objectDatabase.db.get( ddoc._id )
+			.then( doc => {
+				// update if version number has changed
+				if ( this.version !== doc.version ) {
+					console.log("Put new",doc,ddoc);
+					ddoc._rev = doc._rev;
+					ddoc.version = this.version ;
+					return objectDatabase.db.put( ddoc );
+				} else {
+					return Promise.resolve(true);
+				}
+				})
+			.catch( () => {
+				// assume because this is first time and cannot "get"
+				return objectDatabase.db.put( ddoc );
+				});
+			}))
+		.then( _ => this.prune_queries() )
+		.catch( (err) => objectLog.err(err) );
+	}
+	
+	struct_parse(struct) {
+		// create query definision (_design document) by parsing structure and finding:
+		// 1. Query strings
+		// 2. Query strings buried in an array (members)
+		// query gives the name of the search and it is grouped by name
+        return struct.map( e => {
+            if ( "query" in e ) { // primary query field
+//                const f = `function(doc) { if ( "${e.name}" in doc ) { emit(doc.${e.name}) ; }}`;
+                const f = `(doc) => { if ( "${e.name}" in doc ) { emit(doc.${e.name}) ; }}`;
+                return ({
+                    _id: `_design/${e.query}`,
+                    views: {
+                        [e.query]: {
+                            map: f,
+                            reduce: "_count",
+                        },
+                    },
+                }) ;
+            } else if ("members" in e) { // query field in array (or ImageArray)
+                return e.members.filter( m => "query" in m ).map( m => {
+//                    const f = `function(doc) { if ( "${e.name}" in doc ){doc.${e.name}.forEach(g=> { if ( "${m.name}" in g ) { emit(g.${m.name}); }});}};`;
+                    const f = `(doc) => { if ( "${e.name}" in doc ){doc.${e.name}.forEach(g=> { if ( "${m.name}" in g ) { emit(g.${m.name}); }});}};`;
+                    return ({
+                        _id: `_design/${m.query}`,
+                        views: {
+                            [m.query]: {
+                                map: f,
+                                reduce: "_count",
+                            },
+                        },
+                    }) ; 
+                    }) ;
+            } else { // no query -- will filter out
+                return null ;
+            }}).flat().filter( x => x != null ) ;
+	}
+	
+	prune_queries() {
+		// remove old entries (don't match version string)
+		objectDatabase.db.allDocs( {
+            startkey: "_design/",
+            endkey:   "_design/\uffff",
+            include_docs: true,
+        } )
+        .then( docs => docs.rows.filter( r=> r.doc.version !== this.version ) )
+        .then( rows => Promise.all( rows.map( r => objectDatabase.db.remove(r.doc)) ) ) ;
+    }
+
 }
